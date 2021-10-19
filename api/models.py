763fdb5fb188903be6aaa1b223c142a892608e56
Stevinel <72396348+Stevinel@users.noreply.github.com>
2021-10-19 13:41:29 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Game(models.Model):
     """
-    Модель электронного адреса
+    Модель данных игры
     """
 
     email = models.EmailField("Электронный адрес", max_length=50, unique=True)
