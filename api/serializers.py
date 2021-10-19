c50450b443515e9d2618f353600a62558295cb1b
Stevinel <72396348+Stevinel@users.noreply.github.com>
2021-10-19 14:07:05 +0300

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,7 @@ from .models import Game
 
 
 class GameSerializer(serializers.ModelSerializer):
+    """Сериалайзер данных об игре"""
     class Meta:
         model = Game
         fields = ["email", "number_of_games"]
